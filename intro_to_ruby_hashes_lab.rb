f4699315679b12231a322f17b8d09136c050f186
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-04 09:22:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash = { 
-    key => key  
-  }
+  if read_from_hash[hash]
+   read_from_hash[key] += 1
+ else read_from_hash[hash] 1
 end 
   
 
