17359db9d9f6bd7560195c9ae311333d10197b9a
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-04 09:48:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] += 1 
-    return  
-  else hash[key] = 1 
-end
-end
+  if hash[key]  
+    hash[key] += 1 
+  else 
+    hash[key] = 1 
+ end
+hash
+end 
